af1e2e4983d17e5bb434b5d5213d33c95dbf21f8
bryan lynch <bml312@nyu.edu>
2021-05-21 12:24:48 +0800

    refactor crawler into main/worker concurrency

--- src/crawler/spider.clj
+++ src/crawler/spider.clj
@@ -1,48 +1,28 @@
 (ns crawler.spider
-  (:require [clojure.core.async :as a :refer [>! <! >!! <!! go chan buffer go-loop alts! timeout]]
+  (:require [clojure.core.async :as a :refer [>! <! >!! <!! go chan buffer go-loop alts!! timeout]]
             [crawler.parser :as parser]))
 
-(def log-chan (chan 1000))
-;; ;(def site-map (atom #{}))
+(defn worker [url-chan url]
+  (try
+    (let [url-links (parser/get-url-links url)]
+      (go (>! url-chan url-links)))
+    (catch Exception e
+      (println "Error retrieving url: " url))))
 
-(defn log [message]
-  (go (>! log-chan message)))
-
-(defn setup-logger []
-  (go-loop []
-    (println (<! log-chan))
-    (recur)))
-
-(defn create-workers [visited-urls url-chan completion-chan max-workers]
-  (dotimes [_ max-workers]
-    (go-loop [url (<! url-chan)]
-      (when-not (.putIfAbsent visited-urls url true)
-        (log (str "retreiving... " url))
-        (try
-          (let [url-links (parser/get-url-links url)]
-            (doseq [url-link (set url-links)]
-              (go (>! url-chan url-link))))
-          (catch Exception e
-            (log (str "error retreiving url: " url)))))
-      (let [[value channel] (alts! [url-chan (timeout 3000)])]
-        (if (= channel url-chan)
-          (recur value)
-          (>! completion-chan :completed))))))
-
-
-(defn seconds-since [start-time]
-  (let [current-time (System/currentTimeMillis)]
-    (/ (- current-time start-time) 1000.0)))
+(defn crawl [url-chan]
+  (let [visited-urls (atom {})]
+      (loop [urls (<!! url-chan)]
+        (doseq [url urls] 
+          (when-not (contains? @visited-urls url)
+            (println "retrieving..." url)
+            (swap! visited-urls assoc url true)
+            (worker url-chan url))) 
+        (let [[next-urls channel] (alts!! [url-chan (timeout 3000)])]
+          (if (= channel url-chan)
+            (recur next-urls)
+            (keys @visited-urls))))))
 
 (defn crawl-url [url]
-  (let [start-time (System/currentTimeMillis)
-        url-chan (chan 102400)
-        completion-chan (chan 1)
-        visited-urls (java.util.concurrent.ConcurrentHashMap.)
-        ]
-    (setup-logger)
-    (create-workers visited-urls url-chan completion-chan 8)
-    (>!! url-chan url)
-    (println (<!! completion-chan))
-    (println "Completed after: " (seconds-since start-time) " seconds")
-    (keys visited-urls)))
+  (let [url-chan (chan)]
+    (go (>! url-chan (conj [] url)))
+    (crawl url-chan)))
